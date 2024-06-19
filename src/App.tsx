dfdee7c309fd159389dccb47366ff9a26d02f273
akash23-ai <akashbhardwaj2024@gmail.com>
2024-06-19 23:03:41 +0530

    Fixed tailwind bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <div className='max-w-screen-2xl w-full h-screen'>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)} className='button bg-green-600 rounded-md'>
           count is {count}
         </button>
         <p>
@@ -28,7 +17,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
