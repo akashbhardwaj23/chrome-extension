afca40c82afac64fe8906643d8bec3d687b8bc27
akash23-ai <akashbhardwaj2024@gmail.com>
2024-06-22 13:45:07 +0530

    Added some functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,41 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [code, setCode] = useState('')
+
+  const generateCode = () => {
+    const mycode = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    
+    let thisCode = "";
+    for(let i = 0; i < mycode.length; i++){
+      thisCode += mycode.charAt(Math.floor(Math.random() * mycode.length));
+    }
+
+    setCode(thisCode);
+  }
 
   return (
     <div className='max-w-screen-2xl w-full h-screen'>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)} className='button bg-green-600 rounded-md'>
+        <button onClick={() => setCount((count) => count + 1)} className='button bg-green-600 rounded-md mb-2'>
           count is {count}
         </button>
         <button onClick={() => setCount(0)} className='button bg-blue-600 rounded-md'>
           Reset the Count
         </button>
       </div>
+      <p className='text-lg'>
+        This also generate a code for You
+      </p>
+
+      <button className='button my-2 bg-yellow-600 rounded-md' onClick={generateCode}>
+        Generate Code
+      </button>
+
+      <p className='text-xl text-blue-700/90 font-semibold'>
+        {code ?? code}
+      </p>
+
       <p className="read-the-docs">
         This is a Chrome Extension
       </p>
